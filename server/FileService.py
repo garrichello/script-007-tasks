91c78062259f40c006693a6ab8d2fc714736e9ba
Igor Okladnikov <igor.okladnikov@gmail.com>
2022-05-31 10:09:14 +0700

    Add is_pathname_valid to check pathname validity

--- server/FileService.py
+++ server/FileService.py
@@ -12,8 +12,76 @@ Provides functions:
     delete_file()
 """
 from datetime import datetime
+import errno
 import glob
 import os
+
+# Windows-specific error code indicating an invalid pathname.
+ERROR_INVALID_NAME = 123
+
+def is_pathname_valid(pathname: str) -> bool:
+    """
+    Check if pathname is valid.
+
+    Return:
+        True - if the passed pathname is a valid pathname for the current OS;
+        False - otherwise.
+
+    Borrowed here: https://stackoverflow.com/questions/9532499/check-whether-a-path-is-valid-in-python-without-creating-a-file-at-the-paths-ta
+    And slightly modified.
+    """
+
+    # If this pathname is either not a string or is but is empty, this pathname
+    # is invalid.
+    try:
+        if not isinstance(pathname, str) or not pathname:
+            return False
+
+        # Strip this pathname's Windows-specific drive specifier (e.g., `C:\`) if any. 
+        _, pathname = os.path.splitdrive(pathname)
+
+        # Directory guaranteed to exist.
+        root_dirname = os.path.dirname(__file__)
+
+        # Test whether each path component split from this pathname is valid or
+        # not, ignoring non-existent and non-readable path components.
+        for pathname_part in pathname.split(os.path.sep):
+            try:
+                os.lstat(root_dirname + pathname_part)
+            # If an OS-specific exception is raised, its error code
+            # indicates whether this pathname is valid or not. Unless this
+            # is the case, this exception implies an ignorable kernel or
+            # filesystem complaint (e.g., path not found or inaccessible).
+            #
+            # Only the following exceptions indicate invalid pathnames:
+            #
+            # * Instances of the Windows-specific "WindowsError" class
+            #   defining the "winerror" attribute whose value is
+            #   "ERROR_INVALID_NAME". Under Windows, "winerror" is more
+            #   fine-grained and hence useful than the generic "errno"
+            #   attribute. When a too-long pathname is passed, for example,
+            #   "errno" is "ENOENT" (i.e., no such file or directory) rather
+            #   than "ENAMETOOLONG" (i.e., file name too long).
+            # * Instances of the cross-platform "OSError" class defining the
+            #   generic "errno" attribute whose value is either:
+            #   * Under most POSIX-compatible OSes, "ENAMETOOLONG".
+            #   * Under some edge-case OSes (e.g., SunOS, *BSD), "ERANGE".
+            except OSError as exc:
+                if hasattr(exc, 'winerror'):
+                    if exc.winerror == ERROR_INVALID_NAME:
+                        return False
+                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
+                    return False
+    # If a "TypeError" exception was raised, it almost certainly has the
+    # error message "embedded NUL character" indicating an invalid pathname.
+    except TypeError as exc:
+        return False
+    # If no exception was raised, all path components and hence this
+    # pathname itself are valid.
+    else:
+        return True
+    # If any other exception was raised, this is an unrelated fatal issue
+    # (e.g., a bug). Permit this exception to unwind the call stack.
 
 
 def change_dir(path: str, autocreate: bool = True) -> None:
@@ -28,11 +96,11 @@ def change_dir(path: str, autocreate: bool = True) -> None:
         ValueError: if path is invalid.
     """
 
-    try:
-        os.makedirs(path, exist_ok=autocreate)
-    except (OSError, FileNotFoundError):
+    if not is_pathname_valid(path):
         raise ValueError
-
+    if not os.path.exists(path) and not autocreate:
+        raise RuntimeError
+    os.makedirs(path, exist_ok=autocreate)
     os.chdir(path)
 
 
