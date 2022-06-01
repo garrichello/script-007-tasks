0eed1865d3178b53883eac004dfbc65b14321473
Igor Okladnikov <igor.okladnikov@gmail.com>
2022-06-01 17:42:03 +0700

    Cosmetics

--- server/test_fileservice/test_deletefile.py
+++ server/test_fileservice/test_deletefile.py
@@ -20,12 +20,12 @@ class TestDeleteFile:
     def test_bad_file_name(self, bad_file_name):
         """Bad file name raises ValueError"""
         with pytest.raises(ValueError):
-            _ = delete_file(bad_file_name)
+            delete_file(bad_file_name)
 
     def test_file_not_exists(self):
         """Test file does not exists raises RuntimeError"""
         with pytest.raises(RuntimeError):
-            _ = delete_file("non_existing_file")
+            delete_file("non_existing_file")
 
     def test_delete_file(self, tmp_path):
         """Test if get_files returns a list with metadata of a file."""
