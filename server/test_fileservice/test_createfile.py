0958fd04195d104120fcecea45ec28ad87d191fc
Igor Okladnikov <igor.okladnikov@gmail.com>
2022-06-01 18:33:00 +0700

    Add OS dependance in test_bad_file_name

--- server/test_fileservice/test_createfile.py
+++ server/test_fileservice/test_createfile.py
@@ -16,10 +16,15 @@ from ..FileService import create_file
 class TestCreateFile:
     """Test create_file function."""
 
-    def test_bad_file_name(self, bad_file_name):
+    def test_bad_file_name(self, os_system, bad_file_name_win, bad_file_name_lnx):
         """Bad file name raises ValueError"""
         with pytest.raises(ValueError):
-            _ = create_file(bad_file_name, b"")
+            if os_system == "Windows":
+                _ = create_file(bad_file_name_win, b"")
+            elif os_system == "Linux":
+                _ = create_file(bad_file_name_lnx, b"")
+            else:
+                raise NotImplementedError
 
     def test_create_new_binary_file(self, new_binary_file_info, tmp_path, sample_binary_data_1):
         """Test if get_files returns a list with metadata of a file."""
