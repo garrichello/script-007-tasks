af0a3a32638c1a3608d2f4870a1595e131bbcecf
Igor Okladnikov <igor.okladnikov@gmail.com>
2022-06-01 11:09:55 +0700

    Add user story for simple CLI testing

---
+++ functional_tests/test_cli.py
@@ -0,0 +1,20 @@
+"""Functional tests for CLI
+"""
+
+# Alice has heard about a new cool file server app.
+# She downloaded the package from its homepage into a local directory.
+# Since Alice is an experienced user she immediately runs the main module from CLI.
+
+# Alice notices an error message about some required arguments.
+
+# To understand what arguments the main module gets she runs the main module with "-h" argument.
+
+# Now Alice knows that she must provide in CLI a location of the data directory.
+# She starts the app with a required argument "-d" and sees that now app starts without errors.
+
+# Alice is also a very curious person. She wants to try an alternative argument for specifying data directory.
+# She stops the app. And starts it again using "--data-directory" to specify the data directory.
+
+# Again, Alice sees that the app starts without errors.
+
+# Satisfied Alice stops the app and takes a coffee-break.
