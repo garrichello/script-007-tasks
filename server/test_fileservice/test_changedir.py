6b1de904ba235011958edd54a41575afafdd9f61
Igor Okladnikov <igor.okladnikov@gmail.com>
2022-06-03 10:37:36 +0700

    Remove fixtures for directory names

--- server/test_fileservice/test_changedir.py
+++ server/test_fileservice/test_changedir.py
@@ -16,21 +16,6 @@ from ..FileService import change_dir
 class TestChangeDir:
     """Test change_dir function."""
 
-    @pytest.fixture(params=["aNewDir", os.path.join("complex", "dir")])
-    def good_dir(self, request):
-        """Return a good name of a directory."""
-        return request.param
-
-    @pytest.fixture(params=[":a", "///:*this_is_a_bad_dir*:///", "another*bad*dir", ".." + os.path.sep])
-    def bad_dir_win(self, request):
-        """Return a bad name of a directory."""
-        return request.param
-
-    @pytest.fixture(params=["\0", "a" * 300])
-    def bad_dir_lnx(self, request):
-        """Return a bad name of a directory."""
-        return request.param
-
     def test_change_to_existing_dir(self, tmp_path):
         """CWD to an existing directory."""
         target_dir = str(tmp_path)
